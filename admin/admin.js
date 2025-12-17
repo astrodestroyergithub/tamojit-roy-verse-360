2df6d14fa8322e2f1585b5c373db139e21431bfc
Tamojit Roy <tamojitroy140301@gmail.com>
2025-12-17 15:06:33 +0530

    Made some changes

--- admin/admin.js
+++ admin/admin.js
@@ -261,17 +261,6 @@ document
     btn.textContent = "Processing...";
 
     try {
-      /*** Code to be removed completely as per instructions 
-      const { response, data } = await apiCall(
-        "/.netlify/functions/create-newsletter",
-        {
-          method: "POST",
-          body: JSON.stringify(newsletter),
-        }
-      );
-      ***/
-
-      // The above was replaced with the below exact code as per instructions
       const { response, data } = await apiCall(
         "/.netlify/functions/create-newsletter",
         {
@@ -286,7 +275,6 @@ document
           body: JSON.stringify({ newsletter_id: data.id }),
         });
       }
-      // End of code to be used now after removing the old code snippet
 
       if (response.ok) {
         alert("Newsletter created successfully!");
